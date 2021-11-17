71597ace3ab8f812a97fb48acd62fa9876cf6a21
DESKTOP-A1LHGBH\Kamen <s186261@stud.uni-ruse.bg>
2021-11-17 10:40:26 +0200

    no message

---
+++ contacts.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        Здравей 🙂
+    </body>
+</html>
+<a href="index.php">index</a>
---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        Здравей 🙂
+    </body>
+</html>
